302b71f41c0c1fa6256111de410ccaafde54ea6d
Tomas Husar <tomas.husar4401@gmail.com>
2023-11-30 15:25:38 +0100

    added calendar with day selector

--- itu_proj/lib/pages/calendar_page.dart
+++ itu_proj/lib/pages/calendar_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:itu_proj/data/database.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+
 
 class CalendarPage extends StatefulWidget {
   const CalendarPage({super.key});
@@ -13,6 +16,14 @@ class _CalendarPageState extends State<CalendarPage> {
 // refference the hive box
   final _myBox = Hive.box('mybox');
   ToDoDatabase db = ToDoDatabase();
+
+  DateTime today = DateTime.now();
+
+  void _onDaySelected(DateTime day, DateTime focusedDay) {
+    setState(() {
+      today = day;
+    });
+  }
 
   @override
   void initState() {
@@ -28,14 +39,31 @@ class _CalendarPageState extends State<CalendarPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        color: Colors.yellow,
-        child: Center(
-          child: Text(
-            'Calendar Tab Content',
-          ),
-        ),
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          Container(
+              child: TableCalendar(
+                // locale: , // localization stuff
+                rowHeight: 50,
+                headerStyle: HeaderStyle(formatButtonVisible: false, titleCentered: true), // disable the additional button in the header
+                availableGestures: AvailableGestures.all,
+                selectedDayPredicate: (day) => isSameDay(day, today),
+                focusedDay: today,
+                firstDay: DateTime.utc(2010, 10, 16),
+                lastDay: DateTime.utc(2030, 10, 16),
+                onDaySelected: _onDaySelected,
+              ),
+              // color: Colors.yellow,
+              // child: Center(
+              //   child: Text(
+              //     'Calendar Tab Content',
+              //   ),
+              // ),
+            ),
+          Text("selected day =" + today.toString().split(" ")[0]),
+        ],
       ),
     );
   }
