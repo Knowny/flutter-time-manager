93457d3c88725d60450622be6fe4add2e1c74962
terez01 <tereza.magerkova@gmail.com>
2023-12-13 19:54:04 +0100

    timer FE

--- itu_proj/lib/pages/timer_page.dart
+++ itu_proj/lib/pages/timer_page.dart
@@ -1,6 +1,11 @@
+/// file: timer_page.dart
+/// author: xmager00
+/// brief: timer tab
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:itu_proj/data/database.dart';
+import 'package:itu_proj/util/timer_buttons.dart';
 
 class TimerPage extends StatefulWidget {
   const TimerPage({super.key});
@@ -8,11 +13,31 @@ class TimerPage extends StatefulWidget {
   @override
   State<TimerPage> createState() => _TimerPageState();
 }
-
-class _TimerPageState extends State<TimerPage> {
+/// TickerProviderStateMixin for Animation controller vsync
+class _TimerPageState extends State<TimerPage> with TickerProviderStateMixin {
 // refference the hive box
   final _myBox = Hive.box('mybox');
+  //database stufferino
   ToDoDatabase db = ToDoDatabase();
+  //animation controller
+  late AnimationController controller;
+
+  int minutes = 0;
+
+  bool isRunning = false;
+  bool isIncremental = false;
+  bool saveActivity = false;
+
+  //changes time text
+  String get countText {
+    Duration count = controller.duration! * controller.value + Duration(minutes: minutes);
+    //return formatted text in tertiary operator
+    return controller.isDismissed  
+      ? '${controller.duration!.inHours}:${(controller.duration!.inMinutes % 60).toString().padLeft(2, '0')}:${(controller.duration!.inSeconds % 60).toString().padLeft(2, '0')}'
+      : '${count.inHours}:${(count.inMinutes % 60).toString().padLeft(2, '0')}:${(count.inSeconds % 60).toString().padLeft(2, '0')}';
+  }
+  double progress = 1.0;
+  Duration lastTimer = Duration.zero;
 
   @override
   void initState() {
@@ -24,19 +49,203 @@ class _TimerPageState extends State<TimerPage> {
       db.loadData();
     }
     super.initState();
+
+    controller = AnimationController(vsync: this, duration: const Duration(seconds: 0));
+
+    controller.addListener(() {
+      if (controller.isAnimating){
+        setState(() {
+          progress = controller.value;
+        });
+      }else{
+        setState(() {
+          
+          if(controller.isDismissed && !isIncremental){ //decremental ended / stopped
+            //save last duration for repeat
+            lastTimer = controller.duration!;
+            //zero out duration
+            controller.duration = Duration.zero;
+            isRunning = false;
+          }
+          else if (isIncremental && controller.isCompleted){ //incremental ended cycle
+            minutes ++;
+            controller.value = 0;
+            controller.duration = Duration(seconds: 60);
+            controller.forward();
+            isRunning = true; 
+          }
+          else if(isIncremental && controller.isDismissed){ //incremenatal stopped
+            lastTimer = controller.duration! * progress + Duration(minutes: minutes);
+            controller.duration = Duration.zero;
+            isRunning = false;
+            saveActivity = true;
+          }
+          //display full circle
+          progress = 1.0;  
+
+        });
+        
+        
+      }
+    });
+
+
+    @override
+    void dispose(){
+      controller.dispose();
+      super.dispose();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.red,
-        child: Center(
-          child: Text(
-            'Timer Tab Content',
+      backgroundColor: Colors.grey[850],
+      body:  Column(
+        children: [
+          Expanded(
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(40),
+                  child: Text(
+                    'minutes: ${minutes} Last Timer setting: ${lastTimer.inHours}:${(lastTimer.inMinutes % 60).toString().padLeft(2, '0')}:${(lastTimer.inSeconds % 60).toString().padLeft(2, '0')}'
+                  ),
+                ),
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  SizedBox(
+                    width: 300,
+                    height: 300,
+                    child: CircularProgressIndicator(
+                      backgroundColor: Colors.grey[800],
+                      value: progress,
+                      strokeWidth: 5,
+                      
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      //prevents user from picking time while timer is running
+                      if (controller.isDismissed && !isIncremental){
+                        showModalBottomSheet(
+                          context: context, 
+                          builder: (context) => Container(
+                            height: 300,
+                            child: CupertinoTimerPicker(
+                              //initialTimerDuration: controller.duration!,
+                              onTimerDurationChanged: (time) {
+                                setState(() {
+                                  controller.duration = time;
+                                });
+                              },
+                            ),
+                          ),);
+                      }
+                    },
+                    child: AnimatedBuilder(
+                      animation: controller, 
+                      builder: (context, child) => Text(
+                        //from String get countText
+                        countText,
+                        style: TextStyle(
+                          fontSize: 60,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  
+
+                ],
+            ),
+              ],
+            ),
+            
           ),
-        ),
-      ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 40),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    if (controller.isAnimating){
+                      controller.stop();
+                      setState(() {
+                        isRunning = false;
+                      });
+                    }else{
+                      if (isIncremental){
+                        setState(() {
+                          controller.value = 0;
+                          controller.duration = const Duration(seconds: 60);
+                        });
+                        
+                        controller.forward();
+                      }else{
+                        controller.reverse(from: controller.value == 0 ? 1.0 : controller.value);
+                      }
+                      setState(() {
+                        isRunning = true;
+                      });
+                    }
+                  },
+                  
+                  child: RoundButton(
+                    icon: isRunning ? Icons.pause : Icons.play_arrow,
+                    )
+                ),
+                GestureDetector(
+                  onTap:() {
+                    controller.reset();
+                    setState(() {
+                      isRunning = false;
+                    });
+                  },
+                  child: const RoundButton(
+                    icon: Icons.stop
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    if (controller.isDismissed){
+                      controller.duration = lastTimer;
+                      controller.reverse(
+                        from: 1.0
+                      );
+                      setState(() {
+                        isRunning = true;
+                      });
+                    }
+                  },
+                  child: const RoundButton(icon: Icons.restart_alt_rounded),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    if(isIncremental){
+                      setState(() {
+                        isIncremental = false;
+                        minutes = 0;
+                      });
+                      
+                    }else{
+                     setState(() {
+                        isIncremental = true;
+                      });
+                    }
+                  },
+                  child: RoundButton(icon: isIncremental ? Icons.timer : Icons.timelapse),
+                )
+              ],
+            ),
+          )
+        ],
+      )
     );
   }
+
+
 }
+
