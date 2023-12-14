6921abc9e7672fa01d7590a6f0c98aa84895ebdf
Patrik Ješko <xjesko01@vutbr.cz>
2023-12-14 21:14:30 +0100

    should be good 1

--- itu_proj/lib/data/database.dart
+++ itu_proj/lib/data/database.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 var colorMap = {
   "Orange" : Colors.orange,
@@ -29,14 +30,18 @@ class ToDoDatabase {
       ["Push Ups", "Sport", DateTime.now(), 60*10],
       ["Watching Spidrman", "Free Time", DateTime.now(), 60*120],
       ["Talking with Jessica", "Socialising", DateTime.now(), 60*5],
+      ["Watching Witchur", "Free Time", DateTime.now(), 60*30],
+      ["Talking with Jamal", "Socialising", DateTime.now(), 60*10],
+      ["Watching Witchur", "Free Time", DateTime.now(), 60*30],
+      ["Talking with Ferdinand", "Socialising", DateTime.now(), 60*3],
     ];
     
     categoryList = [
     // ["NAME","COLOR"]
-      ["School", "Red", 60*30],
-      ["Sport", "Blue", 60*10],
-      ["Free Time", "Yellow", 60*120],
-      ["Socialising", "Orange", 60*5]
+      ["School", Colors.red, 60*30],
+      ["Sport", Colors.blue, 60*10],
+      ["Free Time", Colors.yellow, 60*120],
+      ["Socialising", Colors.orange, 60*5]
     ];
   }
 
@@ -54,8 +59,13 @@ class ToDoDatabase {
     _myBox.put("CATEGORYLIST", categoryList);
   }
 
-  Color? getCategoryColor(String key){
-    return colorMap[key];
+  Color getCategoryColor(String key){
+    for (var category in categoryList){
+      if (category[0] == key){
+        return category[1];
+      }
+    }
+    return Colors.grey;
   }
 
   num getTotalTimeSpent(){
@@ -64,6 +74,53 @@ class ToDoDatabase {
       timeSpent += category[2];
     }
     return timeSpent;
+  }
+
+  List<dynamic> getActivitiesByDay(DateTime when) {
+    return activityList.where((activity) {
+      DateTime activityDate = activity[2]; // Assuming the date is stored at index 2
+      return isSameDay(activityDate, when);
+    }).toList();
+  }
+
+  num getTodayTotal(String name){
+    num totalTime = 0;
+    for (var index = 0; index < activityList.length; index++) {
+      if (activityList[index][1] == name && isSameDay(activityList[index][2], DateTime.now())){
+        totalTime += activityList[index][3];
+      }
+    }
+    return totalTime;
+  }
+
+  num getThisWeekTotal(String name){
+    num totalTime = 0;
+    for (var index = 0; index < activityList.length; index++) {
+      if (activityList[index][1] == name && isSameWeek(activityList[index][2], DateTime.now())){
+        totalTime += activityList[index][3];
+      }
+    }
+    return totalTime;
+  }
+
+  bool isSameWeek(DateTime date1, DateTime date2) {
+    DateTime mondayDate1 = date1.subtract(Duration(days: date1.weekday - DateTime.monday));
+    DateTime mondayDate2 = date2.subtract(Duration(days: date2.weekday - DateTime.monday));
+    return mondayDate1.day == mondayDate2.day;
+  }
+
+  num getThisMonthTotal(String name){
+    num totalTime = 0;
+    for (var index = 0; index < activityList.length; index++) {
+      if (activityList[index][1] == name && isSameMonth(activityList[index][2], DateTime.now())){
+        totalTime += activityList[index][3];
+      }
+    }
+    return totalTime;
+  }
+
+  bool isSameMonth(DateTime date1, DateTime date2) {
+    return date1.year == date2.year && date1.month == date2.month;
   }
 
 }
