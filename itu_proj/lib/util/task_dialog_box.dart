6de9b908cb0df7c77d0eb792ed677a048ffddbf5
Tomas Husar <tomas.husar4401@gmail.com>
2023-12-15 18:19:44 +0100

    Snackbar Notifications

--- itu_proj/lib/util/task_dialog_box.dart
+++ itu_proj/lib/util/task_dialog_box.dart
@@ -18,6 +18,15 @@ class TaskDialogBox extends StatelessWidget {
     required this.onCancel,
   });
 
+  // * SNACKBAR - TASK ADDED
+  final snackBar = SnackBar(
+    content: Text(
+      'Task added successfully',
+      style: TextStyle(color: Colors.grey.shade900),
+    ),
+    backgroundColor: Colors.lightGreen.withOpacity(0.5),
+  );
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
